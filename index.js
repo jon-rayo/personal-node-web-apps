8e94a4f7f1f82e5ab6de7d8d32c76369527094dc
Jonathan Rayo <jrayo@lewisu.edu>
2022-02-24 11:46:32 -0600

    Add inital files

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+//Define the port
+const port = process.env.PORT || 8080;
+
+//Define directory where static files will be served to run in the browser.
+app.use(express.static(__dirname + '/public'))
+
+app.get('/hello/', (req, res) => {
+    app.send('Hello World!')
+})
+
+const logMessage = 'Application listening on http://localhost:' + port
+app.listen(port, () => {
+    console.log(logMessage)
+})
